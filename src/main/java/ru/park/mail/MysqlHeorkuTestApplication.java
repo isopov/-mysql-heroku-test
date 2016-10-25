c2ee260697f3ce923602b17f9fe79fab212142e0
Ivan Sopov <sopov.ivan@gmail.com>
2016-10-25 16:42:02 +0300

    datasource builder

--- src/main/java/ru/park/mail/MysqlHeorkuTestApplication.java
+++ src/main/java/ru/park/mail/MysqlHeorkuTestApplication.java
@@ -1,10 +1,36 @@
 package ru.park.mail;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
 public class MysqlHeorkuTestApplication {
+
+	@Bean
+	@Primary
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource mainDataSource() throws URISyntaxException {
+		URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+
+		System.out.println(dbUri.getUserInfo().split(":")[0]);
+		System.out.println(dbUri.getUserInfo().split(":")[1]);
+		System.out.println("jdbc:mysql://" + dbUri.getHost() + dbUri.getPath());
+
+		return DataSourceBuilder.create()
+				.username(dbUri.getUserInfo().split(":")[0])
+				.password(dbUri.getUserInfo().split(":")[1])
+				.url("jdbc:mysql://" + dbUri.getHost() + dbUri.getPath())
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MysqlHeorkuTestApplication.class, args);
