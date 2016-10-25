27cfc76c337ca994c9035abd5a7d957227635e1b
Ivan Sopov <sopov.ivan@gmail.com>
2016-10-25 18:37:36 +0300

    JawsDB instead of clearDb

--- src/main/java/ru/park/mail/MysqlHeorkuTestApplication.java
+++ src/main/java/ru/park/mail/MysqlHeorkuTestApplication.java
@@ -15,18 +15,19 @@ import org.springframework.context.annotation.Primary;
 @SpringBootApplication
 public class MysqlHeorkuTestApplication {
 
-	@Bean
-	@Primary
-	@ConfigurationProperties(prefix = "spring.datasource")
-	public DataSource mainDataSource() throws URISyntaxException {
-		URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
-
-		return DataSourceBuilder.create()
-				.username(dbUri.getUserInfo().split(":")[0])
-				.password(dbUri.getUserInfo().split(":")[1])
-				.url("jdbc:mysql://" + dbUri.getHost() + dbUri.getPath())
-				.build();
-	}
+//	@Bean
+//	@Primary
+//	@ConfigurationProperties(prefix = "spring.datasource")
+//	public DataSource mainDataSource() throws URISyntaxException {
+//		// URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+//		URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
+//		String port = String.valueOf(jdbUri.getPort());
+//		return DataSourceBuilder.create()
+//				.username(jdbUri.getUserInfo().split(":")[0])
+//				.password(jdbUri.getUserInfo().split(":")[1])
+//				.url("jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath())
+//				.build();
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MysqlHeorkuTestApplication.class, args);
